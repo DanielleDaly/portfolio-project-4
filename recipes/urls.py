4c7b300b47af1785ea2adc0b8230f3754383e9a2
DanielleDaly <>
2023-05-08 16:03:47 +0000

    Add url path for full recipe

--- recipes/urls.py
+++ recipes/urls.py
@@ -2,5 +2,7 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.RecipeList.as_view(), name='home')
+    path('', views.RecipeList.as_view(), name='home'),
+    path('<slug:slug>/', views.FullRecipe.as_view(), name='full-recipe'),
 ]
+
