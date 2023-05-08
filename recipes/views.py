6a6454a858e006f50b9ed61a4188fe5b3e656505
DanielleDaly <>
2023-05-08 08:45:44 +0000

    Remove pagination from views file & small code tidy up

--- recipes/views.py
+++ recipes/views.py
@@ -2,11 +2,10 @@ from django.shortcuts import render
 from django.views import generic
 from .models import Recipe
 
-class RecipeListView(generic.ListView):
+
+class RecipeList(generic.ListView):
     model = Recipe
     queryset = Recipe.objects.filter(status=1).order_by('-created_on')
-    template_name = index.html
-    paginate_by = 6
-
+    template_name = 'index.html'
 
 
